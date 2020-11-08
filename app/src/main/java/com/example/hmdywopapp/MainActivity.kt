5eae2bb7b5671c92729c2b530cd18474ddfda3be
Fatih Can <ybsfatihcan@gmail.com>
2020-11-08 21:30:02 +0300

    pound fun

--- app/src/main/java/com/example/hmdywopapp/MainActivity.kt
+++ app/src/main/java/com/example/hmdywopapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.hmdywopapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+
+    val KILO_TO_POUND = 2.2045
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userWeight = findViewById<EditText>(R.id.editTextWeight).text
+
+
+    }
+
+    fun kiloToPound(kilo : Double) : Double {
+
+        return kilo * KILO_TO_POUND 
     }
 }
